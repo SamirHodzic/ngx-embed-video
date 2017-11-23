da0f535a4c9a79f199d727da890712de84eada1f
SamirHodzic <samir.sgd@gmail.com>
2017-11-23 16:22:08 +0100

    - introduce karma/jasmine - write specs for service

--- src/embed-video.service.ts
+++ src/embed-video.service.ts
@@ -6,238 +6,238 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class EmbedVideoService {
-	private validYouTubeOptions = [
-		'default',
-		'mqdefault',
-		'hqdefault',
-		'sddefault',
-		'maxresdefault'
-	];
-	private validVimeoOptions = [
-		'thumbnail_small',
-		'thumbnail_medium',
-		'thumbnail_large'
-	];
-	private validDailyMotionOptions = [
-		'thumbnail_60_url',
-		'thumbnail_120_url',
-		'thumbnail_180_url',
-		'thumbnail_240_url',
-		'thumbnail_360_url',
-		'thumbnail_480_url',
-		'thumbnail_720_url',
-		'thumbnail_1080_url'
-	];
+  private validYouTubeOptions = [
+    'default',
+    'mqdefault',
+    'hqdefault',
+    'sddefault',
+    'maxresdefault'
+  ];
+  private validVimeoOptions = [
+    'thumbnail_small',
+    'thumbnail_medium',
+    'thumbnail_large'
+  ];
+  private validDailyMotionOptions = [
+    'thumbnail_60_url',
+    'thumbnail_120_url',
+    'thumbnail_180_url',
+    'thumbnail_240_url',
+    'thumbnail_360_url',
+    'thumbnail_480_url',
+    'thumbnail_720_url',
+    'thumbnail_1080_url'
+  ];
 
-	constructor(
-		private http: Http,
-		private sanitizer: DomSanitizer
-	) { }
+  constructor(
+    private http: Http,
+    private sanitizer: DomSanitizer
+  ) { }
 
-	public embed(url: any, options?: any): any {
-		let id;
-		url = new URL(url);
+  public embed(url: any, options?: any): any {
+    let id;
+    url = new URL(url);
 
-		id = this.detectYoutube(url);
-		if (id) {
-			return this.sanitize_iframe(this.embed_youtube(id, options));
-		}
+    id = this.detectYoutube(url);
+    if (id) {
+      return this.embed_youtube(id, options);
+    }
 
-		id = this.detectVimeo(url);
-		if (id) {
-			return this.sanitize_iframe(this.embed_vimeo(id, options));
-		}
+    id = this.detectVimeo(url);
+    if (id) {
+      return this.embed_vimeo(id, options);
+    }
 
-		id = this.detectDailymotion(url);
-		if (id) {
-			return this.sanitize_iframe(this.embed_dailymotion(id, options));
-		}
-	}
+    id = this.detectDailymotion(url);
+    if (id) {
+      return this.embed_dailymotion(id, options);
+    }
+  }
 
-	public embed_youtube(id: string, options?: any): string {
-		options = this.parseOptions(options);
-		let queryString;
+  public embed_youtube(id: string, options?: any): string {
+    options = this.parseOptions(options);
+    let queryString;
 
-		if (options && options.hasOwnProperty('query')) {
-			queryString = '?' + this.serializeQuery(options.query);
-		}
+    if (options && options.hasOwnProperty('query')) {
+      queryString = '?' + this.serializeQuery(options.query);
+    }
 
-		return '<iframe src="//www.youtube.com/embed/'
-			+ id + options.query + '"' + options.attr
-			+ ' frameborder="0" allowfullscreen></iframe>';
-	}
+    return this.sanitize_iframe('<iframe src="//www.youtube.com/embed/'
+      + id + options.query + '"' + options.attr
+      + ' frameborder="0" allowfullscreen></iframe>');
+  }
 
-	public embed_vimeo(id: string, options?: any): string {
-		options = this.parseOptions(options);
-		let queryString;
+  public embed_vimeo(id: string, options?: any): string {
+    options = this.parseOptions(options);
+    let queryString;
 
-		if (options && options.hasOwnProperty('query')) {
-			queryString = '?' + this.serializeQuery(options.query);
-		}
+    if (options && options.hasOwnProperty('query')) {
+      queryString = '?' + this.serializeQuery(options.query);
+    }
 
-		return '<iframe src="//player.vimeo.com/video/'
-			+ id + options.query + '"' + options.attr
-			+ ' frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-	}
+    return this.sanitize_iframe('<iframe src="//player.vimeo.com/video/'
+      + id + options.query + '"' + options.attr
+      + ' frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+  }
 
-	public embed_dailymotion(id: string, options?: any): string {
-		options = this.parseOptions(options);
-		let queryString;
+  public embed_dailymotion(id: string, options?: any): string {
+    options = this.parseOptions(options);
+    let queryString;
 
-		if (options && options.hasOwnProperty('query')) {
-			queryString = '?' + this.serializeQuery(options.query);
-		}
+    if (options && options.hasOwnProperty('query')) {
+      queryString = '?' + this.serializeQuery(options.query);
+    }
 
-		return '<iframe src="//www.dailymotion.com/embed/video/'
-			+ id + options.query + '"' + options.attr
-			+ ' frameborder="0" allowfullscreen></iframe>';
-	}
+    return this.sanitize_iframe('<iframe src="//www.dailymotion.com/embed/video/'
+      + id + options.query + '"' + options.attr
+      + ' frameborder="0" allowfullscreen></iframe>');
+  }
 
-	public embed_image(url: any, options?: any): any {
-		let id;
+  public embed_image(url: any, options?: any): any {
+    let id;
 
-		url = new URL(url);
+    url = new URL(url);
 
-		id = this.detectYoutube(url);
-		if (id) {
-			return this.embed_youtube_image(id, options);
-		}
+    id = this.detectYoutube(url);
+    if (id) {
+      return this.embed_youtube_image(id, options);
+    }
 
-		id = this.detectVimeo(url);
-		if (id) {
-			return this.embed_vimeo_image(id, options);
-		}
+    id = this.detectVimeo(url);
+    if (id) {
+      return this.embed_vimeo_image(id, options);
+    }
 
-		id = this.detectDailymotion(url);
-		if (id) {
-			return this.embed_dailymotion_image(id, options);
-		}
-	}
+    id = this.detectDailymotion(url);
+    if (id) {
+      return this.embed_dailymotion_image(id, options);
+    }
+  }
 
-	private embed_youtube_image(id: string, options?: any): any {
-		if (typeof options === 'function') {
-			options = {};
-		}
-		options = options || {};
+  private embed_youtube_image(id: string, options?: any): any {
+    if (typeof options === 'function') {
+      options = {};
+    }
+    options = options || {};
 
-		options.image = this.validYouTubeOptions.indexOf(options.image) > 0 ? options.image : 'default';
+    options.image = this.validYouTubeOptions.indexOf(options.image) > 0 ? options.image : 'default';
 
-		let src = '//img.youtube.com/vi/' + id + '/' + options.image + '.jpg';
+    let src = '//img.youtube.com/vi/' + id + '/' + options.image + '.jpg';
 
-		let result = {
-			link: src,
-			html: '<img src="' + src + '"/>'
-		};
+    let result = {
+      link: src,
+      html: '<img src="' + src + '"/>'
+    };
 
-		return new Promise((resolve, reject) => {
-			resolve(result);
-		});
-	}
+    return new Promise((resolve, reject) => {
+      resolve(result);
+    });
+  }
 
-	private embed_vimeo_image(id: string, options?: any): any {
-		if (typeof options === 'function') {
-			options = {};
-		}
+  private embed_vimeo_image(id: string, options?: any): any {
+    if (typeof options === 'function') {
+      options = {};
+    }
 
-		options = options || {};
+    options = options || {};
 
-		options.image = this.validVimeoOptions.indexOf(options.image) >= 0 ? options.image : 'thumbnail_large';
+    options.image = this.validVimeoOptions.indexOf(options.image) >= 0 ? options.image : 'thumbnail_large';
 
-		return this.http.get('https://vimeo.com/api/v2/video/' + id + '.json')
-			.map(res => {
-				return {
-					'link': res.json()[0][options.image],
-					'html': '<img src="' + res.json()[0][options.image] + '"/>'
-				};
-			})
-			.toPromise()
-			.catch(error => console.log(error));
-	}
+    return this.http.get('https://vimeo.com/api/v2/video/' + id + '.json')
+      .map(res => {
+        return {
+          'link': res.json()[0][options.image],
+          'html': '<img src="' + res.json()[0][options.image] + '"/>'
+        };
+      })
+      .toPromise()
+      .catch(error => console.log(error));
+  }
 
-	private embed_dailymotion_image(id: string, options?: any): any {
-		if (typeof options === 'function') {
-			options = {};
-		}
+  private embed_dailymotion_image(id: string, options?: any): any {
+    if (typeof options === 'function') {
+      options = {};
+    }
 
-		options = options || {};
+    options = options || {};
 
-		options.image = this.validDailyMotionOptions.indexOf(options.image) >= 0 ? options.image : 'thumbnail_480_url';
+    options.image = this.validDailyMotionOptions.indexOf(options.image) >= 0 ? options.image : 'thumbnail_480_url';
 
-		return this.http.get('https://api.dailymotion.com/video/' + id + '?fields=' + options.image)
-			.map(res => {
-				return {
-					'link': res.json()[options.image],
-					'html': '<img src="' + res.json()[options.image] + '"/>'
-				};
-			})
-			.toPromise()
-			.catch(error => console.log(error));
-	}
+    return this.http.get('https://api.dailymotion.com/video/' + id + '?fields=' + options.image)
+      .map(res => {
+        return {
+          'link': res.json()[options.image],
+          'html': '<img src="' + res.json()[options.image] + '"/>'
+        };
+      })
+      .toPromise()
+      .catch(error => console.log(error));
+  }
 
-	private parseOptions(options: any): any {
-		let queryString = '',
-			attributes = '';
+  private parseOptions(options: any): any {
+    let queryString = '',
+      attributes = '';
 
-		if (options && options.hasOwnProperty('query')) {
-			queryString = '?' + this.serializeQuery(options.query);
-		}
+    if (options && options.hasOwnProperty('query')) {
+      queryString = '?' + this.serializeQuery(options.query);
+    }
 
-		if (options && options.hasOwnProperty('attr')) {
-			let temp = <any>[];
+    if (options && options.hasOwnProperty('attr')) {
+      let temp = <any>[];
 
-			Object.keys(options.attr).forEach(function (key) {
-				temp.push(key + '="' + (options.attr[key]) + '"');
-			});
+      Object.keys(options.attr).forEach(function (key) {
+        temp.push(key + '="' + (options.attr[key]) + '"');
+      });
 
-			attributes = ' ' + temp.join(' ');
-		}
-		return {
-			query: queryString,
-			attr: attributes
-		};
-	}
+      attributes = ' ' + temp.join(' ');
+    }
+    return {
+      query: queryString,
+      attr: attributes
+    };
+  }
 
-	private serializeQuery(query: any): any {
-		let queryString = [];
+  private serializeQuery(query: any): any {
+    let queryString = [];
 
-		for (let p in query) {
-			if (query.hasOwnProperty(p)) {
-				queryString.push(encodeURIComponent(p) + '=' + encodeURIComponent(query[p]));
-			}
-		}
+    for (let p in query) {
+      if (query.hasOwnProperty(p)) {
+        queryString.push(encodeURIComponent(p) + '=' + encodeURIComponent(query[p]));
+      }
+    }
 
-		return queryString.join('&');
-	}
+    return queryString.join('&');
+  }
 
-	private sanitize_iframe(iframe: string): any {
-		return this.sanitizer.bypassSecurityTrustHtml(iframe);
-	}
+  private sanitize_iframe(iframe: string): any {
+    return this.sanitizer.bypassSecurityTrustHtml(iframe);
+  }
 
-	private detectVimeo(url: any): string {
-		return (url.hostname === 'vimeo.com') ? url.pathname.split('/')[1] : null;
-	}
+  private detectVimeo(url: any): string {
+    return (url.hostname === 'vimeo.com') ? url.pathname.split('/')[1] : null;
+  }
 
-	private detectYoutube(url: any): string {
-		if (url.hostname.indexOf('youtube.com') > -1) {
-			return url.search.split('=')[1];
-		}
+  private detectYoutube(url: any): string {
+    if (url.hostname.indexOf('youtube.com') > -1) {
+      return url.search.split('=')[1];
+    }
 
-		if (url.hostname === 'youtu.be') {
-			return url.pathname.split('/')[1];
-		}
+    if (url.hostname === 'youtu.be') {
+      return url.pathname.split('/')[1];
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	private detectDailymotion(url: any): string {
-		if (url.hostname.indexOf('dailymotion.com') > -1) {
-			return url.pathname.split('/')[2].split('_')[0];
-		}
+  private detectDailymotion(url: any): string {
+    if (url.hostname.indexOf('dailymotion.com') > -1) {
+      return url.pathname.split('/')[2].split('_')[0];
+    }
 
-		if (url.hostname === 'dai.ly') {
-			return url.pathname.split('/')[1];
-		}
+    if (url.hostname === 'dai.ly') {
+      return url.pathname.split('/')[1];
+    }
 
-		return null;
-	}
+    return null;
+  }
 }
---
+++ tests/embed-video.service.spec.ts
@@ -0,0 +1,200 @@
+import { async, inject, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { EmbedVideoService } from '../src/embed-video.service';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+
+describe('EmbedVideoService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpModule
+      ],
+      providers: [
+        EmbedVideoService
+      ]
+    });
+  });
+
+  it('service should be defined',
+    inject([EmbedVideoService], (embedVideoService) => {
+
+      expect(embedVideoService).toBeDefined();
+    }));
+
+  it('converts vimeo.com url',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed('http://vimeo.com/19339941')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts youtube.com url',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed('https://www.youtube.com/watch?v=twE64AuqE9A')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.youtube.com/embed/twE64AuqE9A" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts youtu.be url',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed('http://youtu.be/9XeNNqeHVDw#aid=P-Do3JLm4A0')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.youtube.com/embed/9XeNNqeHVDw" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts dailymotion.com url',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed('https://www.dailymotion.com/video/x20qnej_red-bull-presents-wild-ride-bmx-mtb-dirt_sport')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.dailymotion.com/embed/video/x20qnej" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts dai.ly url',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed('http://dai.ly/x20qnej')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.dailymotion.com/embed/video/x20qnej" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts vimeo id',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_vimeo('19339941')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//player.vimeo.com/video/19339941" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts youtube id',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_youtube('9XeNNqeHVDw')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.youtube.com/embed/9XeNNqeHVDw" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('converts dailymotion id',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_dailymotion('x20qnej')).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.dailymotion.com/embed/video/x20qnej" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('accepts query param youtube',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_youtube('9XeNNqeHVDw', { query: { rel: 0, showinfo: 0 } })).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.youtube.com/embed/9XeNNqeHVDw?rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('accepts attributes youtube',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_youtube('9XeNNqeHVDw', { query: { rel: 0, showinfo: 0 }, attr: { width: 400, height: 200 } })).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.youtube.com/embed/9XeNNqeHVDw?rel=0&showinfo=0" width="400" height="200" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('accepts query param vimeo',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_vimeo('19339941', { query: { portrait: 0, color: '333' } })).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//player.vimeo.com/video/19339941?portrait=0&color=333" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+      )
+    }));
+
+  it('accepts attributes vimeo',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_vimeo('19339941', { query: { portrait: 0, color: '333' }, attr: { width: 400, height: 200 } })).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//player.vimeo.com/video/19339941?portrait=0&color=333" width="400" height="200" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+      )
+    }));
+
+  it('accepts query param dailymotion',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_dailymotion('x20qnej', { query: { autoPlay: 1, start: 66 } })).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.dailymotion.com/embed/video/x20qnej?autoPlay=1&start=66" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('accepts attributes dailymotion',
+    inject([EmbedVideoService, DomSanitizer], (embedVideoService, sanitizer) => {
+
+      expect(embedVideoService.embed_dailymotion('x20qnej', { query: { autoPlay: 1, start: 66 }, attr: { width: 400, height: 200 } })).toEqual(
+        sanitizer.bypassSecurityTrustHtml('<iframe src="//www.dailymotion.com/embed/video/x20qnej?autoPlay=1&start=66" width="400" height="200" frameborder="0" allowfullscreen></iframe>')
+      )
+    }));
+
+  it('gets vimeo thumbnail',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('https://vimeo.com/19339941').then((image) => {
+        expect(image.link).toEqual('https://i.vimeocdn.com/video/122513613_640.jpg');
+        expect(image.html).toEqual('<img src="https://i.vimeocdn.com/video/122513613_640.jpg"/>');
+      });
+    })));
+
+  it('gets vimeo thumbnail with options',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('https://vimeo.com/19339941', { image: 'thumbnail_small' }).then((image) => {
+        expect(image.link).toEqual('https://i.vimeocdn.com/video/122513613_100x75.jpg');
+        expect(image.html).toEqual('<img src="https://i.vimeocdn.com/video/122513613_100x75.jpg"/>');
+      });
+    })));
+
+  it('gets default vimeo thumbnail with invalid options',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('https://vimeo.com/19339941', { image: 'stupid-format' }).then((image) => {
+        expect(image.link).toEqual('https://i.vimeocdn.com/video/122513613_640.jpg');
+        expect(image.html).toEqual('<img src="https://i.vimeocdn.com/video/122513613_640.jpg"/>');
+      });
+    })));
+
+  it('gets youtube thumbnail (prove backwards compatibility)',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('https://youtu.be/ZeLnjXTNq6Q', { image: 'maxresdefault' }).then((image) => {
+        expect(image.link).toEqual('//img.youtube.com/vi/ZeLnjXTNq6Q/maxresdefault.jpg');
+        expect(image.html).toEqual('<img src="//img.youtube.com/vi/ZeLnjXTNq6Q/maxresdefault.jpg"/>');
+      });
+    })));
+
+  it('gets dailymotion thumbnail',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('https://www.dailymotion.com/video/x20qnej_red-bull-presents-wild-ride-bmx-mtb-dirt_sport').then((image) => {
+        expect(image.link).toEqual('http://s1.dmcdn.net/IgPVQ/x480-ktj.jpg');
+        expect(image.html).toEqual('<img src="http://s1.dmcdn.net/IgPVQ/x480-ktj.jpg"/>');
+      });
+    })));
+
+  it('gets dailymotion thumbnail with options',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('https://www.dailymotion.com/video/x20qnej_red-bull-presents-wild-ride-bmx-mtb-dirt_sport', { image: 'thumbnail_720_url' }).then((image) => {
+        expect(image.link).toEqual('http://s1.dmcdn.net/IgPVQ/x720-d_h.jpg');
+        expect(image.html).toEqual('<img src="http://s1.dmcdn.net/IgPVQ/x720-d_h.jpg"/>');
+      });
+    })));
+
+  it('gets dailymotion thumbnail (dai.ly)',
+    async(inject([EmbedVideoService], (embedVideoService) => {
+
+      embedVideoService.embed_image('http://dai.ly/x20qnej').then((image) => {
+        expect(image.link).toEqual('http://s1.dmcdn.net/IgPVQ/x480-ktj.jpg');
+        expect(image.html).toEqual('<img src="http://s1.dmcdn.net/IgPVQ/x480-ktj.jpg"/>');
+      });
+    })));
+});
